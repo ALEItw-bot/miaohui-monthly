8db69a6a6d008222cedf4303957905e85c1e7e48
雷 <tony0917tw@gmail.com>
2026-03-22 11:02:08 +0800

    feat: auto-scan image folders + drag carousel

--- miaohui-monthly/app/layout.tsx
+++ miaohui-monthly/app/layout.tsx
@@ -61,7 +61,6 @@ export default function RootLayout({
                 <a href="/#events" className="header-nav-link">最新消息</a>
                 <a href="/brand-story" className="header-nav-link">品牌故事</a>
                 <a href="/#news" className="header-nav-link">熱鬧資訊</a>
-                <span className="header-nav-dots">..........</span>
               </nav>
             </div>
           </div>
